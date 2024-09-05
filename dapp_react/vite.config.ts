9ea532e840a1759d8f6d9760324417ffe9a9d0a2
chipmunkcol <jack95711@gmail.com>
2024-09-05 15:26:39 +0900

    prerenderer 보류 (body 에 content가 채워지는 만큼 css와 js 모듈 불러오기 전 html 껍데기만 가져오는 이슈 -> 로딩스피너 한다고 해도 다른 /path 에 따른 index.html 을 별도로 가져와줘야 하는데 cloudfront 에 함수 추가 할 수는 있지만 좀 걸릴거같아 일단 보류)

--- dapp_react/vite.config.ts
+++ dapp_react/vite.config.ts
@@ -10,41 +10,41 @@ export default defineConfig({
   plugins: [
     react(),
     svgr(),
-    prerender({
-      routes: [
-        "/",
-        "/market-place",
-        "/market-place/nft",
-        "/market-place/collection",
-        "/create",
-        "/create-mint-nft",
-        "/create-collection/step-1",
-        "/create-collection/step-2",
-        // "/mypage",
-        // "/nft-detail",
-      ],
-      renderer: "@prerenderer/renderer-puppeteer",
-      // rendererOptions: {
-      //   renderAfterDocumentEvent: "custom-render-trigger",
-      // },
-      rendererOptions: {
-        maxConcurrentRoutes: 1,
-        renderAfterTime: 500,
-      },
-      postProcess(renderedRoute) {
-        // Replace all http with https urls and localhost to your site url
-        renderedRoute.html = renderedRoute.html
-          .replace(/http:/gi, "https:")
-          .replace(
-            /(https:\/\/)?(localhost|127\.0\.0\.1):\d*/gi,
-            "dztwi6z9vov5x.cloudfront.net/" || ""
-          );
-        renderedRoute.html = renderedRoute.html.replace(
-          /<style[^>]*>.*?swal2.*?<\/style>/g, // 모든 <style> 태그 제거
-          ""
-        );
-      },
-    }),
+    // prerender({
+    //   routes: [
+    //     "/",
+    //     "/market-place",
+    //     // "/market-place/nft",
+    //     // "/market-place/collection",
+    //     "/create",
+    //     // "/create-mint-nft",
+    //     // "/create-collection/step-1",
+    //     // "/create-collection/step-2",
+    //     // "/mypage",
+    //     // "/nft-detail",
+    //   ],
+    //   renderer: "@prerenderer/renderer-puppeteer",
+    //   // rendererOptions: {
+    //   //   renderAfterDocumentEvent: "custom-render-trigger",
+    //   // },
+    //   rendererOptions: {
+    //     maxConcurrentRoutes: 1,
+    //     renderAfterTime: 500,
+    //   },
+    //   postProcess(renderedRoute) {
+    //     // Replace all http with https urls and localhost to your site url
+    //     renderedRoute.html = renderedRoute.html
+    //       .replace(/http:/gi, "https:")
+    //       .replace(
+    //         /(https:\/\/)?(localhost|127\.0\.0\.1):\d*/gi,
+    //         "dztwi6z9vov5x.cloudfront.net/" || ""
+    //       );
+    //     renderedRoute.html = renderedRoute.html.replace(
+    //       /<style[^>]*>.*?swal2.*?<\/style>/g, // 모든 <style> 태그 제거
+    //       ""
+    //     );
+    //   },
+    // }),
   ],
   resolve: {
     alias: {
